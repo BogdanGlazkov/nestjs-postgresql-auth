ffffc4ccd5830534d2eac3c0d03f568f83a4678a
BogdanGlazkov <bglazkov@i.ua>
2023-07-20 20:46:25 +0300

    Add jwt auth guard

---
+++ src/guards/jwt-auth.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class JwtAuthGuard implements CanActivate {
+  constructor(private readonly jwtService: JwtService) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = request.headers.authorization?.split(' ')[1];
+
+    if (token) {
+      try {
+        const decoded = this.jwtService.verify(token);
+        request.user = decoded;
+        return true;
+      } catch (error) {
+        return false; //Invalid token
+      }
+    }
+    return false; //No token
+  }
+}
