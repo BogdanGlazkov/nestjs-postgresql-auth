7eb207ac3751cd6108b58f901e4884bb78814ebd
BogdanGlazkov <bglazkov@i.ua>
2023-07-20 21:26:08 +0300

    Add user controller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Post } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('create')
+  async createUser(
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    return await this.userService.createUser(email, password);
+  }
+}
