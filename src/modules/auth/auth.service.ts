a12cd3033dc62065663d62cf49b1f17304fe21ed
BogdanGlazkov <bglazkov@i.ua>
2023-07-20 21:46:06 +0300

    Add auth service

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,36 @@
-import { Injectable, Options } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
+import { UserService } from '../user/user.service';
+import { User } from '../../entities';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly userService: UserService,
+  ) {}
+
+  async signUp(email: string, password: string): Promise<User> {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    return this.userService.createUser(email, hashedPassword);
+  }
+
+  async signIn(email: string, password: string): Promise<string> {
+    const user = await this.userService.findOneUser(email);
+    if (!user) {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      throw new HttpException('Invalid credentials', HttpStatus.BAD_REQUEST);
+    }
+
+    return this.jwtService.sign(
+      { userId: user.id, userEmail: user.email },
+      { secret: process.env.JWT_SECRET },
+    );
+  }
+}
