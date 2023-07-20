0627f8ef5d6d62d908a4fb07403c596bbae3b450
BogdanGlazkov <bglazkov@i.ua>
2023-07-20 21:59:12 +0300

    Add auth controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  async signUp(
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    const user = await this.authService.signUp(email, password);
+    return { message: 'User signed up successfully', user };
+  }
+
+  @Post('signin')
+  async signIn(
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    const token = await this.authService.signIn(email, password);
+    return { message: 'User signed in successfully', token };
+  }
+}
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm/dist';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UserService } from '../user/user.service';
+import { User } from '../../entities';
 
 @Module({
-  providers: [AuthService],
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [AuthService, UserService],
   controllers: [AuthController],
 })
 export class AuthModule {}
